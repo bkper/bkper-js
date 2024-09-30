5c86513acb38c462f9fdc7cca2879e18b1054e14
victordrumond <victordrumond@hotmail.com>
2024-09-30 15:45:45 -0300

    Add several methods to Template class

--- src/model/Template.ts
+++ src/model/Template.ts
@@ -16,6 +16,78 @@ export class Template {
     }
 
     /**
+     * Gets the name of the Template.
+     * 
+     * @returns The Template's name
+     */
+    public getName(): string | undefined {
+        return this.wrapped.name;
+    }
+
+    /**
+     * Gets the description of the Template.
+     * 
+     * @returns The Template's description
+     */
+    public getDescription(): string | undefined {
+        return this.wrapped.description;
+    }
+
+    /**
+     * Gets the url of the image of the Template.
+     * 
+     * @returns The url of the Template's image
+     */
+    public getImageUrl(): string | undefined {
+        return this.wrapped.imageUrl;
+    }
+
+    /**
+     * Gets the category of the Template.
+     * 
+     * @returns The Template's category. Example: "PERSONAL", "BUSINESS", etc
+     */
+    public getCategory(): string | undefined {
+        return this.wrapped.category;
+    }
+
+    /**
+     * Gets the times the Template has been used.
+     * 
+     * @returns The number of times the Template has been used
+     */
+    public getTimesUsed(): number {
+        return this.wrapped.timesUsed || 0;
+    }
+
+    /**
+     * Gets the bookId of the [[Book]] associated with the Template.
+     * 
+     * @returns The bookId of the Book associated with the Template
+     */
+    public getBookId(): string | undefined {
+        return this.wrapped.bookId;
+    }
+
+    /**
+     * Gets the link of the [[Book]] associated with the Template.
+     * 
+     * @returns The link of the Book associated with the Template
+     */
+    public getBookLink(): string | undefined {
+        return this.wrapped.bookLink;
+    }
+
+    /**
+     * Gets the link of the Google Sheets spreadsheet associated with the Template.
+     * 
+     * @returns The link of the Google Sheets spreadsheet associated with the Template
+     */
+    public getSheetsLink(): string | undefined {
+        return this.wrapped.sheetsLink;
+    }
+
+    /**
      * Gets the wrapped plain json object of the Template.
      * 
      * @returns The Template wrapped plain json object
