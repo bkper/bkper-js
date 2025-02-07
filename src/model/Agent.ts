cb66b250f17c52cff6894cbbd4a06a591deb2800
victordrumond <victordrumond@hotmail.com>
2025-02-07 14:48:30 -0300

    Add Agent model

---
+++ src/model/Agent.ts
@@ -0,0 +1,47 @@
+/**
+ * Defines an Agent on Bkper.
+ * 
+ * An Agent represents an entity (such as an App or Bot) that interacts with Bkper, executing actions on behalf of users.
+ * 
+ * @public
+ */
+export class Agent {
+
+    public payload: bkper.Agent;
+
+    constructor(payload?: bkper.Agent) {
+        this.payload = payload || {};
+    }
+
+    /**
+     * @returns The wrapped plain json object
+     */
+    public json(): bkper.Agent {
+        return { ...this.payload };
+    }
+
+    /**
+     * 
+     * @returns The Agent universal identifier
+     */
+    public getId(): string | undefined {
+        return this.payload.id;
+    }
+
+    /**
+     * 
+     * @returns The Agent name
+     */
+    public getName(): string | undefined {
+        return this.payload.name;
+    }
+
+    /**
+     * 
+     * @returns The Agent logo url
+     */
+    public getLogoUrl(): string | undefined {
+        return this.payload.logo;
+    }
+
+}
