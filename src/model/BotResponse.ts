6bec2d86923f74612ef24b1cdf7143a5182cfec0
victordrumond <victordrumond@hotmail.com>
2025-02-18 14:59:31 -0300

    Add BotResponse model with basic methods

---
+++ src/model/BotResponse.ts
@@ -0,0 +1,38 @@
+import { BotResponseType } from "./Enums.js";
+
+/**
+ * 
+ * This class defines a Bot Response associated to an [[Event]].
+ * 
+ * @public
+ */
+export class BotResponse {
+
+    public payload: bkper.BotResponse;
+
+    constructor(payload?: bkper.BotResponse) {
+        this.payload = payload || {};
+    }
+
+    /**
+     * @return The type of this Bot Response
+     */
+    public getType(): BotResponseType | undefined {
+        return this.payload.type as BotResponseType | undefined;
+    }
+
+    /**
+     * @return The agent id of this Bot Response
+     */
+    public getAgentId(): string | undefined {
+        return this.payload.agentId;
+    }
+
+    /**
+     * @return The message of this Bot Response
+     */
+    public getMessage(): string | undefined {
+        return this.payload.message;
+    }
+
+}
