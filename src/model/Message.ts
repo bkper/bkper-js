61d3d5ded7d05be4e32a7683929a657b39a2a2f8
victordrumond <victordrumond@hotmail.com>
2025-03-12 09:54:14 -0300

    Add properties methods to Message class

--- src/model/Message.ts
+++ src/model/Message.ts
@@ -34,7 +34,7 @@ export class Message {
      */
     public getId(): string | undefined {
         return this.payload.id;
-    }   
+    }
 
     /**
      * 
@@ -59,7 +59,7 @@ export class Message {
     public getCreatedAt(): Date | undefined {
         return this.payload.createdAt ? new Date(this.payload.createdAt) : undefined;
     }
-        
+
     /**
      * 
      * @returns The text content of the Message
@@ -76,6 +76,77 @@ export class Message {
      */
     public setContent(content: string): Message {
         this.payload.content = content;
+        return this;
+    }
+
+    /**
+     * @returns The custom properties stored in this Message
+     */
+    public getProperties(): { [key: string]: string } {
+        return this.payload.properties != null ? { ...this.payload.properties } : {};
+    }
+
+    /**
+     * Sets the custom properties of the Message
+     * 
+     * @param properties - Object with key/value pair properties
+     * 
+     * @returns This Message, for chainning. 
+     */
+    public setProperties(properties: { [key: string]: string }): Message {
+        this.payload.properties = { ...properties };
+        return this;
+    }
+
+    /**
+     * Gets the property value for given keys. First property found will be retrieved.
+     * 
+     * @param keys - The property key
+     * 
+     * @returns The retrieved property value
+     */
+    public getProperty(...keys: string[]): string | undefined {
+        for (let index = 0; index < keys.length; index++) {
+            const key = keys[index];
+            let value = this.payload.properties != null ? this.payload.properties[key] : null;
+            if (value != null && value.trim() != '') {
+                return value;
+            }
+        }
+        return undefined;
+    }
+
+    /**
+     * Sets a custom property in the Message.
+     * 
+     * @param key - The property key
+     * @param value - The property value
+     * 
+     * @returns This Message, for chainning. 
+     */
+    public setProperty(key: string, value: string | null): Message {
+        if (key == null || key.trim() == '') {
+            return this;
+        }
+        if (this.payload.properties == null) {
+            this.payload.properties = {};
+        }
+        if (!value) {
+            value = '';
+        }
+        this.payload.properties[key] = value;
+        return this;
+    }
+
+    /**
+     * Deletes a custom property from the Message.
+     * 
+     * @param key - The property key
+     * 
+     * @returns This Message, for chainning.
+     */
+    public deleteProperty(key: string): Message {
+        this.setProperty(key, null);
         return this;
     }
 
