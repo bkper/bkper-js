92500ffb9660cc99cc0c4b1cf22a732942034258
Bruno Coelho <bbcoelho@gmail.com>
2025-08-06 22:07:09 -0300

    feat: get balance from balancesUrl

--- src/service/balances-service.ts
+++ src/service/balances-service.ts
@@ -1,7 +1,13 @@
 import { HttpBooksApiV5Request } from "./http-api-request.js";
+import axios from 'axios';
 
 export async function getBalances(bookId: string, query: string): Promise<bkper.Balances> {
   var response = await new HttpBooksApiV5Request(`${bookId}/balances`).addParam('query', query).addParam('time', Date.now()).fetch();
+
+  if (response.data.balancesUrl) {
+    response = await axios.get(response.data.balancesUrl);
+  }
+
   return response.data;
 }
 
