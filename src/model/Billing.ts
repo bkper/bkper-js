090c2d1752eafec887f7017502ad85314e83933d
victordrumond <victordrumond@hotmail.com>
2026-01-19 10:12:39 -0300

    Add Billing model class for user billing information
    - Create new Billing class extending Resource
    - Expose billing status, plan, and transaction metrics
    - Support configuration inheritance from Bkper instance

---
+++ src/model/Billing.ts
@@ -0,0 +1,98 @@
+import { Config } from "./Config.js";
+import { Resource } from "./Resource.js";
+import { Bkper } from "./Bkper.js";
+
+/**
+ * This class defines the Billing information for a [[User]].
+ *
+ * The Billing information includes the plan, the admin email, and the billing portal URL.
+ *
+ * @public
+ */
+export class Billing extends Resource<bkper.Billing> {
+
+    private config?: Config;
+
+    constructor(json?: bkper.Billing, config?: Config) {
+        super(json);
+        this.config = config;
+    }
+
+    /** @internal */
+    public getConfig(): Config {
+        return this.config || Bkper.globalConfig;
+    }
+
+    /**
+     * Tells if billing is enabled for the User.
+     *
+     * @returns True if billing is enabled for the User
+     */
+    public isEnabled(): boolean | undefined {
+        return this.payload.enabled;
+    }
+
+    /**
+     * Gets the current plan of the User.
+     *
+     * @returns The User's plan
+     */
+    public getPlan(): string | undefined {
+        return this.payload.plan;
+    }
+
+    /**
+     * Tells if the User's current plan payment is overdue.
+     *
+     * @returns True if the plan payment is overdue
+     */
+    public isPlanOverdue(): boolean | undefined {
+        return this.payload.planOverdue;
+    }
+
+    /**
+     * Gets the admin email for this User's billing account.
+     *
+     * @returns The billing admin email
+     */
+    public getAdminEmail(): string | undefined {
+        return this.payload.adminEmail;
+    }
+
+    /**
+     * Tells if the User has started the trial period.
+     *
+     * @returns True if the User has started the trial period
+     */
+    public hasStartedTrial(): boolean | undefined {
+        return this.payload.startedTrial;
+    }
+
+    /**
+     * Gets the number of days left in User's trial period.
+     *
+     * @returns The number of days left in trial period
+     */
+    public getDaysLeftInTrial(): number | undefined {
+        return this.payload.daysLeftInTrial;
+    }
+
+    /**
+     * Gets the number of total transactions this month for the User's billing account.
+     *
+     * @returns The number of total transactions this month
+     */
+    public getTotalTransactionsThisMonth(): number | undefined {
+        return this.payload.totalTransactionsThisMonth;
+    }
+
+    /**
+     * Gets the number of total transactions this year for the User's billing account.
+     *
+     * @returns The number of total transactions this year
+     */
+    public getTotalTransactionsThisYear(): number | undefined {
+        return this.payload.totalTransactionsThisYear;
+    }
+
+}
